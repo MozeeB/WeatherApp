f16db2dd7f30468a075309e559d2c0a299c84259
mujibbur741 <mujiburr741@gmail.com>
2021-01-25 09:00:52 +0700

    done

---
+++ android/app/src/main/kotlin/id/cikup/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.cikup.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/service/forecastService.dart
@@ -0,0 +1,13 @@
+
+import 'package:weather_app/model/forecast.dart';
+import 'package:weather_app/service/weatherApi.dart';
+
+class ForecastService {
+  final WeatherApi weatherApi;
+  ForecastService(this.weatherApi);
+
+  Future<Forecast> getWeather(String city) async {
+    final location = await weatherApi.getLocation(city);
+    return await weatherApi.getWeather(location);
+  }
+}
